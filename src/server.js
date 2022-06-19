256e5bcc7cfc98aff80d049f7feee8998c447a63
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-18 22:44:50 -0500

    feat: adding dummy data and reading it on the planets model file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
+
+startServer();
