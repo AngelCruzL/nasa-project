3b720b3f630537de3a2a902b9a9df568a82c25b9
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-19 09:11:22 -0500

    feat: serving '/launch' route as index on builded react project

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.routes');
@@ -6,6 +7,11 @@ const app = express();
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
