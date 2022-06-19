9e9cd618ba1c6ac2c90f60c7d241106ee4313220
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-18 22:22:38 -0500

    feat: getting planets from API

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -1,6 +1,8 @@
+const API_URL = 'http://localhost:8000';
+
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
@@ -18,9 +20,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
