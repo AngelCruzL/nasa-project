a9d63463116d2911f23bcfc68a314d660132ae7e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-19 17:36:12 -0500

    feat: refactor on models - building a data access layer

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -51,7 +51,11 @@ function loadPlanetsData() {
   });
 }
 
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets
+  getAllPlanets
 };
