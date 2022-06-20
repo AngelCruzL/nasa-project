79a089b7aef2f6e96c5fa166afc50a9783b29f5e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-20 13:58:47 -0500

    feat: adding addNewLaunch functionality

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -11,7 +11,7 @@ function httpAddNewLaunch(req, res) {
     !launch.mission ||
     !launch.rocket ||
     !launch.launchDate ||
-    !launch.destination
+    !launch.target
   ) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
