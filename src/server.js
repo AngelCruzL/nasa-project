4bfa45efbd81e99ac25212f3dd8cee7bf812dfbb
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-10 13:46:51 -0500

    feat: add mongoose and db connection

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
 const app = require('./app');
+
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+  console.log('Database online');
+});
+
+mongoose.connection.on('error', err => {
+  console.log({ err });
+});
+
 async function startServer() {
+  await mongoose.connect(process.env.MONGO_URL);
+
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
