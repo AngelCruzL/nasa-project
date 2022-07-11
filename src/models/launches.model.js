cd09bca16035e0e86a6dcb5bb3e39af14821292e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-10 21:54:39 -0500

    feat: mapping SpaceX db response to create launches

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -26,6 +26,25 @@ async function loadLaunchesData() {
       ],
     },
   });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap(payload => payload['customers']);
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} - ${launch.mission}`);
+  }
 }
 
 async function getAllLaunches() {
